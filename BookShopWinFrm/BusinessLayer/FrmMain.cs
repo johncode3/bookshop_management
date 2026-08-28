a3b9252a99d348186bf12b57a04c5f3f4a656510
johncode3 <sovann_kakkada@sr.bbu.edu.kh>
2026-08-28 10:59:05 +0700

    Draft FrmMain.

--- BookShopWinFrm/BusinessLayer/FrmMain.cs
+++ BookShopWinFrm/BusinessLayer/FrmMain.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookShopWinFrm.DataLayer.Model;
+using BookShopWinFrm.DataLayer.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,94 +14,125 @@ namespace BookShopWinFrm.BusinessLayer
 {
     public partial class FrmMain : Form
     {
-        //FrmLogin frmLogin;
-        //FrmCustomer frmCustomer;
-        //FrmSales frmSales;
-        //FrmEmployee frmEmployee;
-        //FrmVendor frmVendor;
-        //FrmInventoryAdjustmnet frmInventoryAdjustmnet;
-        //FrmUser frmUser;
-        //FrmReportCenter frmReportCenter;
-        //DataTable dtUserPermissions;
+        FrmLogin frmLogin;
+        FrmCustomer frmCustomer;
+        FrmSale frmSales;
+        FrmEmployee frmEmployee;
+        FrmVendor frmVendor;
+        FrmInventoryAdjustment frmInventoryAdjustment;
+        FrmUser frmUser;
+        DataTable dtUserPermissions;
 
+        public AppUser UserLogin { get; set; }
         public FrmMain()
         {
             InitializeComponent();
         }
-        //private void FrmMain_Load(object sender, EventArgs e)
-        //{
-        //    frmLogin = new FrmLogin(this);
-        //    if (frmLogin.ShowDialog() == DialogResult.OK)
-        //    {
-        //        LoadUserPermissions();
-        //        if (this.UserLogin.IsAdmin)
-        //        {
-        //            this.mnuAdmin.Visible = true;
-        //        }
-        //        else
-        //        {
-        //            this.mnuAdmin.Visible = false;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        Application.Exit();
-        //    }
-        //}
-        //void LoadUserPermissions()
-        //{
-        //    dtUserPermissions = this.UserLogin.GetUserPermissions();
-        //    foreach (DataRow dr in dtUserPermissions.Rows)
-        //    {
-        //        string permissionName = dr["PermissionName"].ToString();
-        //        switch (permissionName)
-        //        {
-        //            case "Customer":
-        //                this.mnuCustomer.Visible = true;
-        //                break;
-        //            case "Sales":
-        //                this.mnuSales.Visible = true;
-        //                break;
-        //            case "Employee":
-        //                this.mnuEmployee.Visible = true;
-        //                break;
-        //            case "Vendor":
-        //                this.mnuVendor.Visible = true;
-        //                break;
-        //            case "Inventory Adjustment":
-        //                this.mnuInventoryAdjustment.Visible = true;
-        //                break;
-        //            case "Report Center":
-        //                this.mnuReportCenter.Visible = true;
-        //                break;
-        //            default:
-        //                break;
-        //        }
-        //    }
-        //}
-        //private void mnuCustomerCenter_Click(object sender, EventArgs e)
-        //{
-        //    mnuCustomerDropdown.Visible = !mnuCustomerDropdown.Visible;
-        //}
-        //private void submnuCustomer_Click(object sender, EventArgs e)
-        //{
-        //    if (frmCustomer == null)
-        //    {
-        //        frmCustomer = new FrmCustomer();
-        //        frmCustomer.TopLevel = false;
-        //        frmCustomer.FormBorderStyle = FormBorderStyle.None;
-        //        frmCustomer.Dock = DockStyle.Fill;
-        //        frmCustomer.WindowState = FormWindowState.Normal;
-        //        pnlMain.Controls.Add(frmCustomer);
-        //        pnlMain.Tag = frmCustomer;
-        //        frmCustomer.BringToFront();
-        //        frmCustomer.Show();
-        //    }
-        //    else
-        //    {
-        //        frmCustomer.BringToFront();
-        //    }
-        //}
-        //public AppUser UserLogin { get; set; }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+           frmLogin = new FrmLogin(this);
+           if (frmLogin.ShowDialog() == DialogResult.OK)
+           {
+               if (this.UserLogin != null)
+               {
+                   LoadUserPermissions();
+                   
+                   if (this.UserLogin.IsAdmin == 1)
+                   {
+                       this.mnuAdmin.Visible = true;
+                   }
+                   else
+                   {
+                       this.mnuAdmin.Visible = false;
+                   }
+               }
+           }
+           else
+           {
+               Application.Exit();
+           }
+        }
+        void LoadUserPermissions()
+        {
+           dtUserPermissions = AppUserService.GetPermissions(this.UserLogin.AppUserId);
+           
+           if(dtUserPermissions != null && dtUserPermissions.Rows.Count > 0)
+            {
+                foreach(DataRow row in dtUserPermissions.Rows)
+                {
+                    string perm = row["PermissionName"].ToString();
+                    
+                    if (perm == "CustomerView")
+                    {
+                        this.mnuCustomer.Visible = true;
+                    }
+                    if (perm == "SaleView")
+                    {
+                        this.mnuSales.Visible = true;
+                    }
+                    if (perm == "EmployeeView")
+                    {
+                        this.mnuEmployee.Visible = true;
+                    }
+                    if (perm == "VendorView")
+                    {
+                        this.mnuVendor.Visible = true;
+                    }
+                    if (perm == "InventoryAdjustmentView")
+                    {
+                        this.mnuInventoryAdjustment.Visible = true;
+                    }
+                    if (perm == "UserView")
+                    {
+                        this.mnuUser.Visible = true;
+                    }
+                }
+            }
+        }
+        private void mnuCustomerCenter_Click(object sender, EventArgs e)
+        {
+           mnuCustomerDropdown.Visible = !mnuCustomerDropdown.Visible;
+        }
+        private void submnuCustomer_Click(object sender, EventArgs e)
+        {
+           if (frmCustomer == null || frmCustomer.IsDisposed)
+           {
+               frmCustomer = new FrmCustomer();
+               frmCustomer.TopLevel = false;
+               frmCustomer.FormBorderStyle = FormBorderStyle.None;
+               frmCustomer.Dock = DockStyle.Fill;
+               pnlMain.Controls.Add(frmCustomer);
+               pnlMain.Tag = frmCustomer;
+               frmCustomer.BringToFront();
+               frmCustomer.Show();
+           }
+           else
+           {
+               frmCustomer.BringToFront();
+           }
+        }
+        private void mnuSalesCenter_Click(object sender, EventArgs e)
+        {
+            mnuSalesDropdown.Visible = !mnuSalesDropdown.Visible;
+        }
+        private void submnuSales_Click(object sender, EventArgs e)
+        {
+            if (frmSales == null || frmSales.IsDisposed)
+            {
+                frmSales = new FrmSale();
+                frmSales.TopLevel = false;
+                frmSales.FormBorderStyle = FormBorderStyle.None;
+                frmSales.Dock = DockStyle.Fill;
+                pnlMain.Controls.Add(frmSales);
+                pnlMain.Tag = frmSales;
+                frmSales.BringToFront();
+                frmSales.Show();
+            }
+            else
+            {
+                frmSales.BringToFront();
+            }
+        }
     }
 }
