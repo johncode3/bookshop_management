811170ac52841d112f1dcbd2e1ed11f0bb97183e
Sovann Kakkada <sovann_kakkada@sr.bbu.edu.kh>
2026-09-04 09:11:06 +0700

    fix sql command

--- BookShopWinFrm/BusinessLayer/FrmMain.Designer.cs
+++ BookShopWinFrm/BusinessLayer/FrmMain.Designer.cs
@@ -28,18 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1132, 88);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ControlDarkDark;
+            panel2.Controls.Add(panel4);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 88);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(319, 637);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ControlLight;
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(319, 88);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(813, 637);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.ActiveCaption;
+            panel4.Controls.Add(pictureBox1);
+            panel4.Dock = DockStyle.Top;
+            panel4.Location = new Point(0, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(319, 57);
+            panel4.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(10, 10);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(37, 40);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1299, 687);
+            ClientSize = new Size(1132, 725);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "FrmMain";
             Text = "FrmMain";
+            panel2.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel4;
+        private PictureBox pictureBox1;
+        private Panel panel3;
     }
 }
